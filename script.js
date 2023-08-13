5daab27dc8b8c2d643daacec2498425110294e77
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-14 01:22:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-function validEmail(str) {
-  //your JS code here.
-}
+// This file is not to be modified. Please ignore this.
+// We will understand all of this later in the course.
+// DO NOT MODIFY THIS FILE
 
-// Do not change the code below.
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/main.html'));
+});
+//your code here
+app.post('/add', (req, res) => {
+  const {a,b} = req.body;
+  res.status(200).send(a+b);
+  // res.sendFile(path.join(__dirname + '/main.html'));
+});
+module.exports = app;
